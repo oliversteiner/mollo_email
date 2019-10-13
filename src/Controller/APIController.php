c2995f1062198ec7ca2411569c0eebbfbf90af61
Oliver Steiner <oliver@mollo.ch>
2019-10-13 23:40:29 +0200

    fix invalid email / telemetry member

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\mollo_email\Controller;
 
+use Drupal;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\mollo_email\Utility\ImapTrait;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -39,14 +40,33 @@ class APIController extends ControllerBase
   {
     $name = 'invalid';
     $base = '/api/email/';
-    $version = '1.0.5';
+    $version = '1.0.9';
     $dir = DRUPAL_ROOT.'/mails/';
 
     //  $addresses = ['info@test.com', 'example@example.com'];
 
+    // Get list form Config
+    // Build Settings Name
+    $addresses_config = [];
+
+    $config = Drupal::service('config.factory')->getEditable('smmg_newsletter.settings');
+    $config_invalid_email_string = $config->get('invalid_email');
+    $list = explode(',', $config_invalid_email_string);
+    foreach ($list as $mail) {
+      $addresses_config[] = trim($mail);
+    }
+
     $addresses_IMAP = self::getAllInvalidAddresses();
     [$addresses_FIELES, $exclude_list] = self::getEmailsFromDirectory($dir);
-    $addresses =array_merge($addresses_FIELES, $addresses_IMAP);
+    $_addresses =array_merge($addresses_FIELES, $addresses_IMAP, $addresses_config);
+    $addresses = array_unique($_addresses);
+    sort($addresses);
+
+
+    // Save new List to Config
+    $config->set('invalid_email', implode(', ',$addresses));
+    $config->save();
+
 
     $response = [
       'name' => $base.$name,
